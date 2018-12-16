bf493b6a7bc8fdad91ab626a9439e20a5c663717
Pedro Abranches de Carvalho <abranchesjr@gmail.com>
2018-12-16 19:29:03 +0100

    Q4 and Q5

--- src/Visualization.py
+++ src/Visualization.py
@@ -1,10 +1,19 @@
-import plotly.graph_objs as go
-import pandas as pd
 import math
+import operator
 import numpy as np
-import cufflinks as cf
+import pandas as pd
+
+import plotly
+import plotly.graph_objs as go
+import plotly.plotly as py
+
+from plotly.tools import FigureFactory as FF
 from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
 from IPython.display import display, HTML
+from plotly.grid_objs import Grid, Column
+
+
+plotly.tools.set_credentials_file(username='StudentUni', api_key='8EBJTXg2lf04Yxb03QEw')
 
 def visualization_q2(data_q2_final):
 
@@ -131,3 +140,264 @@ def visualization_q2(data_q2_final):
     fig = go.Figure(data=data, layout=layout)
     iplot(fig)
     return
+
+def visualization_Q4(dataframe):
+    """ Make visualization for question 4
+        Input:
+            dataframe
+        Output:
+            plotly image
+    """
+
+    df_0 = dataframe
+    years_from_col = set(df_0['year'])
+    years_ints = sorted(list(years_from_col))
+    years = [str(year) for year in years_ints]
+
+    # make list of continents
+    continents = []
+    for continent in df_0['region']:
+        if continent not in continents: 
+            continents.append(continent)
+
+    columns = []
+    # make grid
+    for year in years:
+        for continent in continents:
+            dataset_by_year = df_0[df_0['year'] == year]
+            dataset_by_year_and_cont = dataset_by_year[dataset_by_year['region'] == continent]
+            
+            for col_name in dataset_by_year_and_cont:
+                # each column name is unique
+                column_name = '{year}_{continent}_{header}_gapminder_grid'.format(
+                    year=year, continent=continent, header=col_name
+                )
+                a_column = Column(list(dataset_by_year_and_cont[col_name]), column_name)
+                columns.append(a_column)
+
+    # upload grid
+    grid = Grid(columns)
+    url = py.grid_ops.upload(grid, 'gapminder_grid'+str(time.time()), auto_open=False)
+
+    figure = {
+        'data': [],
+        'layout': {},
+        'frames': [],
+        'config': {'scrollzoom': True}
+    }
+
+    # fill in most of layout
+    figure['layout']['xaxis'] = {'title': 'Number of events per month by country', 'gridcolor': '#FFFFFF'}
+    figure['layout']['yaxis'] = {'title': 'Emotion charge', 'range':[0,0.25], 'gridcolor': '#FFFFFF'}
+    figure['layout']['hovermode'] = 'closest'
+    figure['layout']['plot_bgcolor'] = 'rgb(223, 232, 243)'
+
+
+    figure['layout']['sliders'] = {
+        'active': 0,
+        'yanchor': 'top',
+        'xanchor': 'left',
+        'currentvalue': {
+            'font': {'size': 20},
+            'prefix': 'text-before-value-on-display',
+            'visible': True,
+            'xanchor': 'right'
+        },
+        'transition': {'duration': 300, 'easing': 'cubic-in-out'},
+        'pad': {'b': 10, 't': 50},
+        'len': 0.9,
+        'x': 0.1,
+        'y': 0,
+        'steps': [...]
+        }
+    {
+        'method': 'animate',
+        'label': 'label-for-frame',
+        'value': 'value-for-frame(defaults to label)',
+        'args': [{'frame': {'duration': 300, 'redraw': False},
+             'mode': 'immediate'}
+        ],
+    }
+    sliders_dict = {
+        'active': 0,
+        'yanchor': 'top',
+        'xanchor': 'left',
+        'currentvalue': {
+            'font': {'size': 20},
+            'prefix': 'Year:',
+            'visible': True,
+            'xanchor': 'right'
+        },
+        'transition': {'duration': 300, 'easing': 'cubic-in-out'},
+        'pad': {'b': 10, 't': 50},
+        'len': 0.9,
+        'x': 0.1,
+        'y': 0,
+        'steps': []
+    }
+    figure['layout']['updatemenus'] = [
+        {
+            'buttons': [
+                {
+                    'args': [None, {'frame': {'duration': 500, 'redraw': False},
+                             'fromcurrent': True, 'transition': {'duration': 300, 'easing': 'quadratic-in-out'}}],
+                    'label': 'Play',
+                    'method': 'animate'
+                },
+                {
+                    'args': [[None], {'frame': {'duration': 0, 'redraw': False}, 'mode': 'immediate',
+                    'transition': {'duration': 0}}],
+                    'label': 'Pause',
+                    'method': 'animate'
+                }
+            ],
+            'direction': 'left',
+            'pad': {'r': 10, 't': 87},
+            'showactive': False,
+            'type': 'buttons',
+            'x': 0.1,
+            'xanchor': 'right',
+            'y': 0,
+            'yanchor': 'top'
+        }
+    ]          
+
+
+    col_name_template = '{year}_{continent}_{header}_gapminder_grid'
+    year = '2015_02'
+    for continent in continents:
+        data_dict = {
+            'xsrc': grid.get_column_reference(col_name_template.format(
+                year=year, continent=continent, header='CountCountrySource'
+            )),
+            'ysrc': grid.get_column_reference(col_name_template.format(
+                year=year, continent=continent, header='NormEvnt'
+            )),
+            'mode': 'markers',
+            'textsrc': grid.get_column_reference(col_name_template.format(
+                year=year, continent=continent, header='CountrySource'
+            )),
+            'marker': {
+                'sizemode': 'area',
+                'sizeref': 200000,
+                'sizesrc': grid.get_column_reference(col_name_template.format(
+                        year=year, continent=continent, header='SizeToViz'
+                    ))
+            },
+            'name': continent
+        }
+        figure['data'].append(data_dict)
+
+
+    for year in years:
+        frame = {'data': [], 'name': str(year)}
+        for continent in continents:
+            data_dict = {
+                'xsrc': grid.get_column_reference(col_name_template.format(
+                    year=year, continent=continent, header='CountCountrySource'
+                )),
+                'ysrc': grid.get_column_reference(col_name_template.format(
+                    year=year, continent=continent, header='NormEvnt'
+                )),
+                'mode': 'markers',
+                'textsrc': grid.get_column_reference(col_name_template.format(
+                    year=year, continent=continent, header='CountrySource'
+                )),
+                'marker': {
+                    'sizemode': 'area',
+                    'sizeref': 200000,
+                    'sizesrc': grid.get_column_reference(col_name_template.format(
+                        year=year, continent=continent, header='SizeToViz'
+                    ))
+            },
+            'name': continent
+            }
+            frame['data'].append(data_dict)
+
+        figure['frames'].append(frame)
+        slider_step = {'args': [
+            [year],
+            {'frame': {'duration': 300, 'redraw': False},
+             'mode': 'immediate',
+           'transition': {'duration': 300}}
+         ],
+         'label': year,
+         'method': 'animate'}
+        sliders_dict['steps'].append(slider_step)
+
+    figure['layout']['sliders'] = [sliders_dict]
+
+    py.icreate_animations(figure)
+
+
+def visualization_Q5(S,all_dicts,Countries_L):
+    """ Make visualization for question 5
+        Input:
+            dataframe and dictionary with list of dictionaries
+        Output:
+            plotly image
+    """
+
+    # list of words that are not readable to avoid
+    words_avoid=["ly","verbs","noun","prep","verb","ed","object","polit","quan","vice","neverness","form","econ","coll",
+                "pron","modif","acad","be","s","we","freq","ends","qual","indef","gen","conj","period","trait"]
+
+    # initialize lists for x and y data, as well as years and data for the plotly image
+    plot_y=[]
+    plot_x=[]
+    years=[]
+    data=[]
+
+    # indexes of countries we want to analize
+    Countries_indexes = S.index[S['country_source'].isin(Countries_L)].tolist()
+
+    # initialize counter
+    init_country = S["country_source"][Countries_indexes[0]]
+
+    # array of the word positions to analyze
+    random_array = [3,50,270]
+
+    # iterate thorugg the indexes
+    for n in Countries_indexes:
+        numb = n
+        # sort the list
+        sorted_d = sorted(all_dicts[numb][1].items(), key=operator.itemgetter(1), reverse=True)
+
+        # iterate through array of indexes
+        for t in random_array:
+            if sorted_d[t][0] in words_avoid:
+                pass
+            else:
+                plot_y.append(sorted_d[t][1]/(all_dicts[numb][0])*100)
+                plot_x.append(sorted_d[t][0])
+                years.append(S["SizeViz"][n])
+
+        if init_country != S["country_source"][n] or n == len(Countries_indexes) -1:
+            trace = go.Scatter(
+                    x=plot_x,
+                    y=plot_y,
+                    name = S["country_source"][n],
+                    mode = 'markers',
+                    marker = dict(
+                        size = years,
+                        opacity = 0.5
+                    )
+                )
+            data.append(trace)
+            
+            plot_y=[]
+            plot_x=[]
+            years=[]
+            
+            init_country = S["country_source"][n]
+        
+
+    layout = go.Layout(
+        legend=dict(x=-.1, y=1,orientation="h"),
+        title='Percentage of words used by countries',
+        yaxis=dict( title='Percentage of the word in speech'),
+        xaxis=dict( title='Words used')
+)
+    fig = go.Figure(data=data, layout=layout)
+    
+    return fig
--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,124 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+#################################################### Milestone 3 helper functions
+
+## data processing
+def concat_parquets_info(directory,drop_flag,Q4=False,Q5=False):
+	""" Get dataframe from parquets
+		Input:
+		    directory of files
+		Output:
+		    dataframe with al parquets information
+	"""	
+
+	# list of months and years that we read from the cluster GDELT
+	Years_Months = ["2015_02","2015_03","2015_04","2015_05","2015_06",
+                "2015_07","2015_08","2015_09","2015_10","2015_11","2015_12",
+                
+                "2016_01","2016_02","2016_03","2016_04","2016_05","2016_06",
+                "2016_07","2016_08","2016_09","2016_10","2016_11","2016_12",
+                
+                "2017_01","2017_02","2017_03","2017_04","2017_05","2017_06",
+                "2017_07","2017_08","2017_09","2017_10","2017_11"]
+
+	if Q4 == True:
+		df_counter = 0
+		for filename in os.listdir(directory):
+		    if df_counter == 0:
+		        df_0 = pd.read_csv(directory + '/' + filename,sep='\t', encoding='utf-8')
+		        df_0 = df_0.drop(['Unnamed: 0'], axis=1)
+		        df_0["year"] = Years_Months[df_counter]
+		    else:
+		        df_1 = pd.read_csv(directory + '/' + filename,sep='\t', encoding='utf-8')
+		        df_1 = df_1.drop(['Unnamed: 0'], axis=1)
+		        df_1["year"] = Years_Months[df_counter]
+		        df_0 = df_0.append(df_1)
+		    df_counter += 1
+
+
+	if Q5 == True:
+	    # create counter to know the year and motnh of the parquet
+		df_counter = 0
+		for filename in os.listdir(directory):
+			# initialize dataframe
+		    if df_counter == 0:
+		        df_0 = pd.read_csv(directory + '/' + filename,sep='\t', encoding='utf-8')
+		        df_0 = df_0.drop(['Unnamed: 0'], axis=1)
+		        df_0["year"] = filename[0:4]
+
+		    # append the others dataframe to the initialized one before
+		    else:
+		        df_1 = pd.read_csv(directory + '/' +filename,sep='\t', encoding='utf-8')
+		        df_1 = df_1.drop(['Unnamed: 0'], axis=1)
+		        df_1["year"] = filename[0:4]
+		        df_0 = df_0.append(df_1)
+		    df_counter += 1
+
+		if drop_flag == True:
+			# reset the index and drop the duplicates
+			df_0 = df_0.drop_duplicates(subset = ["CountrySource","year"])
+		df_0.reset_index(drop=True)
+
+	return df_0
+
+def build_dict(dataframe):
+	""" Get dictionary for words used
+		Input:
+		    dataframe with the information of the parquet files
+		Output:
+		    list with dictionary and what words and how many times they were used.
+		    the words is still the code and not the words themselves
+	"""	
+	all_dicts = []
+	# iterate through lists of words
+	for out_list in dataframe["SpeechWordsList"]:
+		# create a dicitonary for them
+	    out_list = [s.strip(" ") for s in out_list]
+	    dict_t = {}
+
+	    # counter to know how many words were used
+	    word_counter = 0
+	    for emot_code in out_list:
+	        if emot_code not in dict_t:
+	            dict_t[emot_code] = 1
+	        else:
+	            dict_t[emot_code] += 1
+	        word_counter += 1
+
+	    # append to final list
+	    all_dicts.append((word_counter,dict_t))
+
+	return all_dicts
+
+def dict_with_words(all_dicts,dataframe,HRE):
+	""" Get dictionary with human readable words
+		Input:
+		    dataframe and dictionary with list of dictionaries
+		Output:
+		    the same list of dictionaries but with words instead of code for words
+	"""
+
+	for i in range(len(dataframe)):
+	    dnumb = i
+	    try:
+	        del all_dicts[dnumb][1][""]
+	    except:
+	        pass
+	    copy_dict = all_dicts[dnumb][1].copy()
+
+	    for key in copy_dict.keys():
+	        new_key = HRE.loc[HRE['Variable'] == key, "DimensionHumanName"].iloc[0]
+	        all_dicts[dnumb][1][new_key] = all_dicts[dnumb][1][key]
+	        del all_dicts[dnumb][1][key]
+
+	return all_dicts
+
+
+
+#################################################### Milestone 2 helper functions
 def get_emotion_dictionary(data_dir, GCAM_txt):
 	""" Get dictionary for emotions
 		Input:
